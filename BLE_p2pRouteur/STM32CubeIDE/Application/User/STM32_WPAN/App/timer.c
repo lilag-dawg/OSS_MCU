e6c02d42b1d99815201bd78a49b8dc9aaa777ec4
Carl <carl.moreau@usherbrooke.ca>
2020-11-22 15:50:50 -0500

    Timer for sensor data
    ti
    
    
    ti
    
    
    t
    
    
    t

---
+++ BLE_p2pRouteur/STM32CubeIDE/Application/User/STM32_WPAN/App/timer.c
@@ -0,0 +1,48 @@
+/*
+ * timer.c
+ *
+ *  Created on: 22 nov. 2020
+ *      Author: Carl
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "timer.h"
+/* Private typedef -----------------------------------------------------------*/
+/* Public functions ----------------------------------------------------------*/
+
+
+void startSensorsTimer()
+{
+
+	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+	TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+	htim2.Instance = TIM2;
+	htim2.Init.Prescaler = PRESCALER_VALUE;
+	htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim2.Init.Period = 4294967295;
+	htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+	{
+	Error_Handler();
+	}
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+	{
+	Error_Handler();
+	}
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+	{
+	Error_Handler();
+	}
+
+	__HAL_TIM_ENABLE(&htim2);
+}
+
+uint32_t getSensorsTime()
+{
+	return __HAL_TIM_GET_COUNTER(&htim2);
+}
